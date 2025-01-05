a9f0a9696fc3f8351480eadb8f020ac0ab1527a0
Khushi88800 <Khushisheel425@gmail.com>
2025-01-05 11:27:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+     build: {
+    rollupOptions: {
+      output: {
+        // Configure how files are organized in the output folder
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 server: {
     host: '0.0.0.0',
     port: 5173,
